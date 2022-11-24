2f199d961a0c44aa3d0818083923e3d604b7070a
nimasrorun <sayangnimas14@gmail.com>
2022-11-24 20:30:47 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/autentifikasi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.autentifikasi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:autentifikasi/LoginScreen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:autentifikasi/LoginScreen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
